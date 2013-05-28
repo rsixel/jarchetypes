a538ef006f3975321eafc0f41a213cbd2f5d91da
Ricardo Sixel <ricardo@somatore.com>
2013-05-28 13:20:15 -0300

    Correção na montagem da combo.

--- jarchetypes-scanner/src/main/java/org/jarchetypes/scanner/SelectOneMenuScanner.java
+++ jarchetypes-scanner/src/main/java/org/jarchetypes/scanner/SelectOneMenuScanner.java
@@ -1,5 +1,6 @@
 package org.jarchetypes.scanner;
 
+import java.io.File;
 import java.lang.reflect.Field;
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
@@ -17,62 +18,75 @@ import org.jarchetypes.descriptor.WidgetDescriptor;
 
 /**
  * @author Fabio Sebastiao
- *
+ * 
  */
 public class SelectOneMenuScanner extends InputTextScanner {
 
 	private static final String TEMPLATE_NAME = "org/jarchetypes/scanner/templates/selectOneMenu.vm";
+	private static final String SELECT_ONE_OBJECT_CONVERTER_TEMPLATE_NAME = "org/jarchetypes/scanner/templates/selectOneUsingObjectConverter.vm";
+
+	private static Boolean generated = false;
 
 	static {
 		SelectOneMenuScanner scanner = new SelectOneMenuScanner();
 		register(SelectOneMenu.class, scanner);
-		//registerDefaultType(String.class, scanner);// define o registro pad�o
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	protected void doScan(Class<?> archetype, Member member,
 			VelocityContext context) {
+		synchronized (generated) {
+			if (!generated) {
+				generate(context);
+				generated = true;
+			}
+		}
+
 		WidgetDescriptor descriptor = new WidgetDescriptor();
 
-		SelectOneMenu annotation = (SelectOneMenu) getAnnotation(SelectOneMenu.class,
-				member);
+		SelectOneMenu annotation = (SelectOneMenu) getAnnotation(
+				SelectOneMenu.class, member);
 
 		String name = ArchetypesUtils.uncaptalize(ArchetypesUtils
 				.getFieldName(member));
 		descriptor.setTemplateName(TEMPLATE_NAME);
 		descriptor.setTitle(annotation != null ? annotation.title()
 				: ArchetypesUtils.captalize(name));
-		
-		
-		ParameterizedType parametezed = (ParameterizedType)(member instanceof Method ? ((Method) member)
+
+		ParameterizedType parametezed = (ParameterizedType) (member instanceof Method ? ((Method) member)
 				.getGenericReturnType() : ((Field) member).getGenericType());
 		Class<?> ob = (Class<?>) parametezed.getActualTypeArguments()[0];
-		
+
 		String nameclass = ob.getName();
 		int len = ob.getSimpleName().length();
-        String out = "";
-        out += ob.getSimpleName().substring( 0, 1 ).toLowerCase();
-        out += ob.getSimpleName().substring( 1,len );
+		String out = "";
+		out += ob.getSimpleName().substring(0, 1).toLowerCase();
+		out += ob.getSimpleName().substring(1, len);
 		descriptor.setFieldName(out);
 		descriptor.setFieldType(nameclass);
-		ListFilterDescriptor listFilterDescriptor2 = new ListFilterDescriptor(descriptor);
-		if (getAnnotation(ListFilter.class, member) != null)
-			((List<ListFilterDescriptor>) context.get("listFilters"))
-					.add(listFilterDescriptor2);
-		
-		
+		ListFilterDescriptor listFilterDescriptor2 = new ListFilterDescriptor(
+				descriptor);
+
+		List<ListFilterDescriptor> listFilters = (List<ListFilterDescriptor>) context
+				.get("listFilters");
+
+		if (getAnnotation(ListFilter.class, member) != null) {
+			listFilters.add(listFilterDescriptor2);
+		}
+
 		descriptor.setFieldName(name);
 		descriptor.setFieldObject(out);
 		descriptor.setBeanName(ArchetypesUtils.uncaptalize(archetype
 				.getSimpleName()));
-				
+
 		descriptor.setFieldType(member instanceof Method ? ((Method) member)
-				.getGenericReturnType().toString() : ((Field) member).getGenericType().toString());
-		
-		ListFilterDescriptor listFilterDescriptor = new ListFilterDescriptor(descriptor);
-		
-		
+				.getGenericReturnType().toString() : ((Field) member)
+				.getGenericType().toString());
+
+		ListFilterDescriptor listFilterDescriptor = new ListFilterDescriptor(
+				descriptor);
+
 		listFilterDescriptor.setBeanName(ArchetypesUtils.uncaptalize(archetype
 				.getSimpleName()) + "SearchBean");
 
@@ -84,100 +98,125 @@ public class SelectOneMenuScanner extends InputTextScanner {
 		scanForItems(archetype, member, descriptor);
 		scanForStyle(archetype, member, descriptor);
 		scanForSelectItems(archetype, member, descriptor);
-		
+
 		if (getAnnotation(ListFilter.class, member) != null)
-			((List<ListFilterDescriptor>) context.get("listFilters"))
-					.add(listFilterDescriptor);
-		
+			listFilters.add(listFilterDescriptor);
+
 	}
-	
+
+	protected void generate(VelocityContext context) {
+		try {
+			String outputPath = (String) context.get("outputPath");
+			String sourceDirectory = (String) context.get("sourceDirectory");
+
+			String targetPackagePath = ((String) context.get("targetPackage"))
+					.replace(".", File.separator);
+
+			generate(SELECT_ONE_OBJECT_CONVERTER_TEMPLATE_NAME, sourceDirectory
+					+ File.separator + targetPackagePath,
+					"SelectOneUsingObjectConverter", ".java", context);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public void scanForConverter(Class<?> archetype, Member member,
-			WidgetDescriptor descriptor){
-		try{
-			SelectOneMenu selectOneMenu = (SelectOneMenu) getAnnotation(SelectOneMenu.class,
-					member);
-			if(selectOneMenu!=null){
+			WidgetDescriptor descriptor) {
+		try {
+			SelectOneMenu selectOneMenu = (SelectOneMenu) getAnnotation(
+					SelectOneMenu.class, member);
+			if (selectOneMenu != null) {
 				descriptor.setAttribute("converter", selectOneMenu.converter());
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void scanForVar(Class<?> archetype, Member member,
-			WidgetDescriptor descriptor){
-		try{
-			SelectOneMenu selectOneMenu = (SelectOneMenu) getAnnotation(SelectOneMenu.class,
-					member);
-			if(selectOneMenu!=null){
+			WidgetDescriptor descriptor) {
+		try {
+			SelectOneMenu selectOneMenu = (SelectOneMenu) getAnnotation(
+					SelectOneMenu.class, member);
+			if (selectOneMenu != null) {
 				descriptor.setAttribute("var", selectOneMenu.var());
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void scanForRequired(Class<?> archetype, Member member,
-			WidgetDescriptor descriptor){
-		try{
-			SelectOneMenu selectOneMenu = (SelectOneMenu) getAnnotation(SelectOneMenu.class,
-					member);
-			if(selectOneMenu!=null){
-				descriptor.setAttribute("required", selectOneMenu.required()?"true":"false");
+			WidgetDescriptor descriptor) {
+		try {
+			SelectOneMenu selectOneMenu = (SelectOneMenu) getAnnotation(
+					SelectOneMenu.class, member);
+			if (selectOneMenu != null) {
+				descriptor.setAttribute("required",
+						selectOneMenu.required() ? "true" : "false");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void scanForRequiredMessage(Class<?> archetype, Member member,
-			WidgetDescriptor descriptor){
-		try{
-			SelectOneMenu selectOneMenu = (SelectOneMenu) getAnnotation(SelectOneMenu.class,
-					member);
-			if(selectOneMenu!=null){
-				descriptor.setAttribute("requiredMessage", selectOneMenu.requiredMessage());
+			WidgetDescriptor descriptor) {
+		try {
+			SelectOneMenu selectOneMenu = (SelectOneMenu) getAnnotation(
+					SelectOneMenu.class, member);
+			if (selectOneMenu != null) {
+				descriptor.setAttribute("requiredMessage",
+						selectOneMenu.requiredMessage());
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void scanForItems(Class<?> archetype, Member member,
-			WidgetDescriptor descriptor){
-		try{
-			SelectOneMenu selectOneMenu = (SelectOneMenu) getAnnotation(SelectOneMenu.class,
-					member);
-			if(selectOneMenu!=null){
-				descriptor.setAttribute("items", selectOneMenu.items()?"true":"false");
+			WidgetDescriptor descriptor) {
+		try {
+			SelectOneMenu selectOneMenu = (SelectOneMenu) getAnnotation(
+					SelectOneMenu.class, member);
+			if (selectOneMenu != null) {
+				descriptor.setAttribute("items", selectOneMenu.items() ? "true"
+						: "false");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void scanForStyle(Class<?> archetype, Member member,
-			WidgetDescriptor descriptor){
-		try{
-			SelectOneMenu selectOneMenu = (SelectOneMenu) getAnnotation(SelectOneMenu.class,
-					member);
-			if(selectOneMenu!=null){
+			WidgetDescriptor descriptor) {
+		try {
+			SelectOneMenu selectOneMenu = (SelectOneMenu) getAnnotation(
+					SelectOneMenu.class, member);
+			if (selectOneMenu != null) {
 				descriptor.setAttribute("style", selectOneMenu.style());
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void scanForSelectItems(Class<?> archetype, Member member,
-			WidgetDescriptor descriptor){
-		try{
-			SelectOneMenu selectOneMenu = (SelectOneMenu) getAnnotation(SelectOneMenu.class,
-					member);
-			if(selectOneMenu!=null){
-				descriptor.setAttribute("selectItems", selectOneMenu.selectItems()!=null?"true":"false");
-				descriptor.setAttribute("selectItemsVar", selectOneMenu.selectItems().var()!=null?selectOneMenu.selectItems().var():"");
-				descriptor.setAttribute("itemLabel", selectOneMenu.selectItems().itemLabel()!=null?selectOneMenu.selectItems().itemLabel():"");
+			WidgetDescriptor descriptor) {
+		try {
+			SelectOneMenu selectOneMenu = (SelectOneMenu) getAnnotation(
+					SelectOneMenu.class, member);
+			if (selectOneMenu != null) {
+				descriptor.setAttribute("selectItems",
+						selectOneMenu.selectItems() != null ? "true" : "false");
+				descriptor.setAttribute("selectItemsVar", selectOneMenu
+						.selectItems().var() != null ? selectOneMenu
+						.selectItems().var() : "");
+				descriptor.setAttribute("itemLabel", selectOneMenu
+						.selectItems().itemLabel() != null ? selectOneMenu
+						.selectItems().itemLabel() : "");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
